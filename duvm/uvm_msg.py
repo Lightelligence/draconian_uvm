7e4a7f84f4987d697a3a12e4732e449123fb567a
Hao Wu <hao.wu@lightelligence.ai>
2021-02-04 10:50:56 -0500

    update after Will's review

--- duvm/uvm_msg.py
+++ duvm/uvm_msg.py
@@ -1,4 +1,4 @@
-"""Ban $display in favor of `uvm_info
+"""Ban using uvm_* message macros in favor of cmn_*
 
 """
 # Python Imports
@@ -16,21 +16,16 @@ class UvmMsg(filters.LineListener):
                     filters.TestLineBroadcaster,
                     filters.UVCLineBroadcaster]
 
-    uvmmsg_re = re.compile(r"^\s*`uvm\_(.+?)\(")
+    uvmmsg_re = re.compile(r"^\s*`uvm\_((info)|(debug)|(warning)|(error)|(fatal))\(")
     verbose_re = re.compile(r"UVM\_(.*)\)")
 
     def _update(self, line_no, line):
         match = self.uvmmsg_re.search(line)
-        str = "fixme-hw:inside uvm_msg"
-        #print(str)
-        #print(line)
         if match:
-            #print(match.group(1))
             uvmmacrotype = match.group(1)
             if (uvmmacrotype == 'info'):
                 verbose_match = self.verbose_re.search(line)
                 if verbose_match:
-                    #print(verbose_match.group(1))
                     uvmverbose_type = verbose_match.group(1)
                     if (uvmverbose_type == 'LOW'):
                         self.error(line_no, line, "Usage of uvm_{} is not recommended, please use cmn_info() or cmn_dbg() instead.".format(match.group(1)))
@@ -38,17 +33,12 @@ class UvmMsg(filters.LineListener):
                         self.error(line_no, line, "Usage of uvm_{} is not recommended, please use cmn_dbg() instead.".format(match.group(1)))
                 else :
                     self.error(line_no, line, "Usage of uvm_{} is not recommended, please use cmn_info() instead.".format(match.group(1)))
-
-            if (uvmmacrotype == 'warning'):
+            elif (uvmmacrotype == 'warning'):
                 self.error(line_no, line, "Usage of uvm_{} is not recommended, please use cmn_warn() instead.".format(match.group(1)))
-
-            if (uvmmacrotype == 'error'):
+            elif (uvmmacrotype == 'error'):
                 self.error(line_no, line, "Usage of uvm_{} is not recommended, please use cmn_err() instead.".format(match.group(1)))
-
-            if (uvmmacrotype == 'fatal'):
+            elif (uvmmacrotype == 'fatal'):
                 self.error(line_no, line, "Usage of uvm_{} is not recommended, please use cmn_fatal() instead.".format(match.group(1)))
-
-
 
     update_testbenchtopline = _update
     update_testline = _update
