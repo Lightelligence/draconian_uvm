7c248d270bc194032fa06bafcd7b78e9d919fc34
Guoqi Lu <guoqi.lu@lightelligence.ai>
2022-07-27 10:22:29 -0400

    missed a merge conflict, missed one file

--- tests/test_fassert_randomize.py
+++ tests/test_fassert_randomize.py
@@ -61,7 +61,7 @@ class AssertRandomizeTestCase(test.TestCase):
     def test_randomize_wrapped(self):
         """Randomize call is wrapped correctly with cmn_assert"""
         content = StringIO("""
-        `cmn_assert(cfg.randomize());
+        `cmn_fassert(cfg.randomize());
         """)
         with mock.patch.object(self.cut, "error", autospec=True):
             lb = lbc("/tests/base_test.sv",
@@ -75,7 +75,7 @@ class AssertRandomizeTestCase(test.TestCase):
     def test_randomize_wrapped_with(self):
         """Randomize call with with statement is wrapped correctly with cmn_assert."""
         content = StringIO("""
-        `cmn_assert(cfg.randomize() with { period_ps == 1000});
+        `cmn_fassert(cfg.randomize() with { period_ps == 1000});
         """)
         with mock.patch.object(self.cut, "error", autospec=True):
             lb = lbc("/tests/base_test.sv",
@@ -104,7 +104,7 @@ class AssertRandomizeTestCase(test.TestCase):
     def test_comment_ok(self):
         """Commented out case"""
         content = StringIO("""
-        // `cmn_assert(std::randomize(seed))
+        // `cmn_fassert(std::randomize(seed))
         """)
         with mock.patch.object(self.cut, "error", autospec=True):
             lb = lbc("/tests/base_test.sv",
